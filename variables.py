a51600b30aa6335a546bfad0419aa2183c645606
santiiago18 <scardaya@myuax.com>
2026-02-13 14:21:11 +0100

    Tipos variable

---
+++ variables.py
@@ -0,0 +1,15 @@
+vi = 2
+print ("vi:",vi,"la , añade automaticamente un espacio")
+print ("type(i):",type(vi))
+
+vf = 2.0
+print ("vf:",vf)
+print ("type(vf):",type(vf))
+
+vb = True #Ejemplo de comentario. En python True y False se escribe con la primera letra en mayuscula.
+print ("vb:",vb)
+print ("type(vb):",type(vb))
+
+vs = "Hola"
+print ("vs:",vs)
+print ("type(vs):",type(vs))
